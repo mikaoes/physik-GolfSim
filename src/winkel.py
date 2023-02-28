d442c5276d44289472075539b7e757f4d73beef4
Mika <66172894+mikaoes@users.noreply.github.com>
2023-02-28 22:11:48 +0000

    fix src/winkel.py : probleme mit grad- und bogenms

--- src/winkel.py
+++ src/winkel.py
@@ -1,13 +1,13 @@
 import math
 
-def vx(v, alpha):
-    return v * math.cos(alpha)
+def vx(v, alp):
+    return round(v * math.cos(math.radians(alp)), 2)
 
-def vy(v, alpha):
-    return v * math.sin(alpha)
+def vy(v, alp):
+    return round(v * math.sin(math.radians(alp)), 2)
 
-def Fx(m, v, alpha):
-    return m * v * math.cos(alpha)
+def Fx(m, v, alp):
+    return round(m * v * math.cos(math.radians(alp)), 2)
 
-def Fy(m, v, alpha):
-    return m * v * math.sin(alpha)
+def Fy(m, v, alp):
+    return round(m * v * math.sin(math.radians(alp)), 2)
